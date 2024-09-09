b91fed05aa197d8300173961af55771c8af43a9f
Matija Zezelj <matija.zezelj@elev.ga.ntig.se>
2024-09-09 09:38:30 +0200

    Fråga 1

--- Frågesport/Program.cs
+++ Frågesport/Program.cs
@@ -1,1 +1,26 @@
-﻿
+﻿{
+    
+    {
+        // Håller reda på rätta svar
+        int poäng = 0;
+
+        // Fråga 1
+        Console.WriteLine("Fråga 1: Vilken är Sveriges huvudstad?");
+        Console.WriteLine("1: Malmö");
+        Console.WriteLine("2: Göteborg");
+        Console.WriteLine("3: Stockholm");
+        string svar1 = Console.ReadLine();
+
+        if (svar1 == "3")
+        {
+            Console.WriteLine("Rätt!");
+            poäng++;
+        }
+        else
+        {
+            Console.WriteLine("Fel! Rätt svar är 3: Stockholm.");
+        }
+
+
+
+
