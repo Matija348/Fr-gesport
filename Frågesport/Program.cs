54a01846975f748c2be72e1f0c51ef4b5ea3b481
Matija Zezelj <matija.zezelj@elev.ga.ntig.se>
2024-09-09 09:39:47 +0200

    Fråga 2

--- Frågesport/Program.cs
+++ Frågesport/Program.cs
@@ -20,7 +20,22 @@
         {
             Console.WriteLine("Fel! Rätt svar är 3: Stockholm.");
         }
+// Fråga 2
+        Console.WriteLine("\nFråga 2: Vilket är det största djuret på jorden?");
+        Console.WriteLine("1: Elefant");
+        Console.WriteLine("2: Blåval");
+        Console.WriteLine("3: Krokodil");
+        string svar2 = Console.ReadLine();
 
+        if (svar2 == "2")
+        {
+            Console.WriteLine("Rätt!");
+            poäng++;
+        }
+        else
+        {
+            Console.WriteLine("Fel! Rätt svar är 2: Blåval.");
+        }
 
 
 
